666e1dac50e7e61a67884781d3817f8e7fa025a1
Gianfranco De Paula <baciliogianfranco@gmail.com>
2022-08-07 22:02:20 -0400

    Agregando cambios en landing

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,12 @@ module.exports = {
     extend: {
       colors: {
         new: "#FF5733",
+        gray: "#d3d3d3",
+        dark_blue: "#003652",
+        dark_green: "#15677d",
+        light_blue: "#0fb4ec",
+        dark_gray: "#696969",
+        blue_form: "#3f37c9"
       },
     },
   },
